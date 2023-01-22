41ef017fd7c5fb41a853e7499169ea3e9e16893c
Miriam Retka <miriamretka@gmail.com>
2023-01-22 11:12:00 -0300

    Initial commit

---
+++ TODO.md
@@ -0,0 +1,9 @@
+- [ ] Set up Jest with Typescript in the sandbox
+- [ ] Match package versions
+- [ ] Create reusable template
+- [ ] Configure ESLint rules
+- [ ] EXTRA: Configure Apollo to send GQL queries
+- [ ] EXTRA: Set up a way to mock receiving GQL query responses
+
+I wonder if we can use something similar for
+Rails apps with GraphQL???
---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+#app
+  section.sheet.padding-10mm
+    Brand
+    // Your custom components go here
+    hr
+    h1 Vue App template
+    .head1 Vue Templates
+    ul
+      li Using pug
+  section.sheet.padding-10mm
+    p Some other stuff...
+    p More stuff
+</template>
+
+<script lang="ts">
+import Brand from "@/components/Head";
+
+export default {
+  components: { Brand },
+};
+</script>
+
+<style lang="scss" scoped>
+@page {
+  size: A5;
+}
+
+.head1 {
+  text-align: center;
+}
+</style>
